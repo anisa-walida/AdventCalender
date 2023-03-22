79531b5641e00e695d90554edac8b939ac66d042
anisa_walida <anisawalida206@gmail.com>
2023-03-23 00:21:38 +0600

    Updated

---
+++ app/src/test/java/myCalender.java
@@ -0,0 +1,24 @@
+public class myCalender {
+    int a,b;
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public myCalender(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+}
